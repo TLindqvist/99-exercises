5c3c2100d83f538429500311ac6b0c911d139ea2
Tobias Lindqvist <tobias_mejl@hotmail.se>
2023-11-16 12:29:46 +0100

    Exercise 3 - 5

--- Program.fs
+++ Program.fs
@@ -1,13 +1,11 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
-let rec last x =
-    match x with
+let rec last = function
     | [] -> None
     | [x] -> Some x
     | _::xs -> last xs
 
-let rec last_two xs =
-    match xs with
+let rec last_two = function
     | []
     | [_] -> None
     | [x;y] -> Some(x,y)
@@ -16,3 +14,32 @@ let rec last_two xs =
 let lista = [1;2;3;4]
 
 printfn "%O" <| last_two lista
+
+let nth n list =
+    let rec innerFn index list' =
+        match (index, list') with
+        | 1, x::_ -> Some x
+        | _, [] -> None
+        | _, _::xs -> innerFn (index-1) xs 
+    innerFn n list
+
+printfn "%O" <| nth 3 lista
+
+
+let length list = 
+    let rec innerFn n = function
+        | [] -> n
+        | _::xs -> innerFn (n+1) xs
+    innerFn 0 list
+
+printfn "%O" <| length lista
+
+let reverse list =
+    let rec innerFn rList list' =
+        match list' with
+        | [] -> rList
+        | x::xs -> innerFn (x::rList) xs
+    innerFn [] list
+
+printfn "%O" <| reverse lista
+
